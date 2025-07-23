b39f56684e56df098edf72df20d0a9e36ab297c7
drr00t <adribeiro@gmail.com>
2025-07-22 22:46:16 -0300

    examples refresh

--- fluss-examples/README.md
+++ fluss-examples/README.md
@@ -1,27 +1,55 @@
 <!--
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 -->
+
+Fluss is designed to address the demands of real-time analytics with the following key capabilities:
+
+- Real-Time Stream Reading and Writing: Supports millisecond-level end-to-end latency.
+- Columnar Stream: Optimizes storage and query efficiency.
+- Streaming Updates: Enables low-latency updates to data streams.
+- Changelog Generation: Supports changelog generation and consumption.
+- Real-Time Lookup Queries: Facilitates instant lookup queries on primary keys.
+- Streaming & Lakehouse Unification: Seamlessly integrates streaming and lakehouse storage for unified data processing.
+
 
 # Fluss Examples
 
-This directory contains example code for using Fluss.
+This is a curated list of examples that showcase event streaming using Apache Pulsar.
 
 # Prerequisites
 
 - Docker + Compose installed on your machine.
+
+```bash
+# cd fluss-examples
+docker compose up
+```
+
+## Full example list
+
+- [Java Client Admin](clientjava-admin/clientjava-admin.md)
+- [Java Client Basic Writing](clientjava-basic-write/clientjava-basic-write.md)
+- [Basic Schema](clientjava-basic-schema/clientjava-basic-schema.md)
+- [Log Table Example](clientjava-log-table/clientjava-logtable.md)
+- [PrimaryKey Table Example](clientjava-primarykey-table/clientjava-primarykey-table.md)
+- [Table Bucketing](table-bucketing/table-bucketing.md)
+- [Table Partitions](table-partitioning/table-partitioning.md)
+
+### PubSub
 
 ## Running the Environment
 
---
+++ fluss-examples/clientjava-basic-write/src/main/java/com/alibaba/fluss/examples/JavaClientAdminExample.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.fluss.examples;
+
+import com.alibaba.fluss.client.Connection;
+import com.alibaba.fluss.client.ConnectionFactory;
+import com.alibaba.fluss.client.admin.Admin;
+import com.alibaba.fluss.client.table.Table;
+import com.alibaba.fluss.config.Configuration;
+import com.alibaba.fluss.metadata.DatabaseDescriptor;
+import com.alibaba.fluss.metadata.Schema;
+import com.alibaba.fluss.metadata.TableDescriptor;
+import com.alibaba.fluss.metadata.TablePath;
+import com.alibaba.fluss.types.DataTypes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/** Example Java Client usage for Fluss demonstrating streaming operations. */
+public class JavaClientAdminExample {
+    private static final Logger logger = LoggerFactory.getLogger(JavaClientAdminExample.class);
+
+    public static void main(String[] args) throws Exception {
+
+        logger.info("Starting {}", JavaClientAdminExample.class.getSimpleName());
+
+        String hostname = "";
+        String channel = "ENV";
+
+        if (System.getenv("FLUSS_HOST") != null) {
+            hostname = System.getenv("FLUSS_HOST");
+        }
+
+        if (args.length > 0) {
+            logger.info("Default server host changed via CLI to: {}", args[0]);
+            hostname = args[0];
+            channel = "CLI";
+        }
+
+        if (hostname.isEmpty()) {
+            hostname = "localhost:9123";
+            channel = "DEFAULT";
+        }
+
+        logger.info("Default server host setup via {} to: {}", hostname, channel);
+
+        // creating Connection object to connect with Fluss cluster
+        Configuration conf = new Configuration();
+        conf.setString("bootstrap.servers", hostname);
+
+        Table table;
+        try (Connection connection = ConnectionFactory.createConnection(conf)) {
+
+            // Create database descriptor
+            DatabaseDescriptor databaseDescriptor =
+                    DatabaseDescriptor.builder()
+                            .comment("This is a test database for Fluss Java Client")
+                            .customProperty("owner", "data-team")
+                            .build();
+
+            Schema schema =
+                    Schema.newBuilder()
+                            .column("key", DataTypes.STRING())
+                            .column("value", DataTypes.INT())
+                            .build();
+
+            // Use the schema in a table descriptor
+            TableDescriptor tableDescriptor = TableDescriptor.builder().schema(schema).build();
+
+            // obtain Admin instance from the Connection
+            Admin admin = connection.getAdmin();
+
+            // Create database (true means ignore if exists)
+            admin.createDatabase("my_db", databaseDescriptor, true).get();
+
+            TablePath tablePath = TablePath.of("my_db", "basic_table");
+
+            admin.createTable(tablePath, tableDescriptor, true).get();
+
+            // obtain Table instance from the Connection
+            table = connection.getTable(tablePath);
+            logger.info("Table created successfully {}", table.getTableInfo());
+
+            admin.dropTable(tablePath, false).get();
+            logger.info("Table dropped successfully {}", tablePath);
+        }
+    }
+}
